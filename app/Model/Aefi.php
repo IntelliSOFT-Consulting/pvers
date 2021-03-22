747bdf4517569daa4eaa0aa66061733d1b516f57
zipate <eddieokemwa@gmail.com>
2021-03-22 09:12:15 +0300

    mshangwe

--- app/Model/Aefi.php
+++ app/Model/Aefi.php
@@ -61,7 +61,7 @@ class Aefi extends AppModel {
     }
 
     public function findByHealthProgram($data = array()) {
-        $vdrugs = ClassRegistry::init('DrugDictionary')->find('list', array('conditions' => array('health_program' => $data['health_program']), 'fields' => array('drug_name', 'drug_name')));
+        $vdrugs = ClassRegistry::init('Vaccine')->find('list', array('conditions' => array('health_program' => $data['health_program']), 'fields' => array('vaccine_name', 'vaccine_name')));
         $cond = array($this->alias.'.id' => $this->AefiListOfVaccine->find('list', array(
             'conditions' => array(
                 'OR' => array(
